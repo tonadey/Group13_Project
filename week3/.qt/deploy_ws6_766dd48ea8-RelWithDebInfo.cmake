a1747661eb4049efe22fd696d1216fb033ddc7fb
Oluwatoniloba Adeyinka <svyoa2@nottingham.ac.uk>
2026-04-28 11:59:50 +0100

    Delete week3/.qt directory

--- week3/.qt/deploy_ws6_766dd48ea8-RelWithDebInfo.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/tonad/test/Group13_Project/week3/.qt/QtDeploySupport-RelWithDebInfo.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ws6-plugins-RelWithDebInfo.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/tonad/test/Group13_Project/week3/RelWithDebInfo/ws6.exe"
-    GENERATE_QT_CONF
-)
