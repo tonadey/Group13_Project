1d6b27eceb7a00028a193132a8f167f0a01cccb1
Bekr Alshukry <eeyba6@nottingham.ac.uk>
2026-04-28 11:43:03 +0100

    Delete week3/.qt directory

--- week3/.qt/deploy_ws6_766dd48ea8-Debug.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/tonad/test/Group13_Project/week3/.qt/QtDeploySupport-Debug.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ws6-plugins-Debug.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/tonad/test/Group13_Project/week3/Debug/ws6.exe"
-    GENERATE_QT_CONF
-)
