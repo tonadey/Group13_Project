8321fc9df8bb84bd4ba7d95f811944cad53b8c67
Bekr Alshukry <eeyba6@nottingham.ac.uk>
2026-04-30 17:20:17 +0100

    basic NSIS installer for code pre vr application

--- week3/main.cpp
+++ week3/main.cpp
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[]) {
   // Set up VTK OpenGL surface format
   QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
-
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
